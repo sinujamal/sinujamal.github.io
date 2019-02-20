4949ef9fffa692ac27054897d7399ff9b40aae6a
sinujamal <sinujamal@gmail.com>
2019-02-20 07:29:22 +0530

    Initial commit

---
+++ js/helper.js
@@ -0,0 +1,59 @@
+var base_url = "http://localhost/zt";
+
+var updateTrainingList = function(parentDom, ltstListFlg)
+{	//alert(trng_lst);
+//	trng_lst_Obj = JSON.parse(trng_lst);	
+	var training_item_dom_str=""
+	var count = 0;
+	for(i in trng_lst)
+	{
+		training_item_dom_str += '<div class="training_item col_1_3" >'+									
+									'<img src="' + trng_lst[i].img +'">'+									
+									'<h3>' + trng_lst[i].title + '</h3>'+
+									'<p>' + trng_lst[i].desc + '</p>'+									
+								 '</div>';
+		if( (ltstListFlg == true) && (++count >= 3))
+			break;
+	}
+	
+	if(training_item_dom_str != "" && ltstListFlg == true) 
+	{
+		training_item_dom_str +=  '<div class="col_1"> <a href="./pages/alltraiinings.html" class="more">'+
+									'<span> More </span>'+
+								  '</a></div>'
+	}
+	if(training_item_dom_str == "")
+	{
+		training_item_dom_str +=  '<div>'+
+									'<h1> No trainings found </h1>'+
+								  '</div>'
+	}
+	parentDom.innerHTML = training_item_dom_str;
+}
+
+
+var showHeader = function(showFlg){
+	var mainCntnr = document.getElementById('main-container');
+	var prevInnerHTML = mainCntnr.innerHTML;
+	var hdrStr = '<header class="header">'+
+		'<a href="#"> <img src="'+base_url+'/img/logo.png"> </img></a>'+
+		'<nav><ul class="menu">'+
+				'<li><a href="#">Your menu </a></li>'+
+				'<li><a href="#">Your menu </a></li>'+
+				'<li><a href="#">Your menu </a></li>'+
+			'</ul>'+
+		'</nav>'+
+	'</header>';
+	
+	mainCntnr.innerHTML = hdrStr + prevInnerHTML;	
+};
+
+var showFooter = function(showFlg){
+	var mainCntnr = document.getElementById('main-container');
+	var prevInnerHTML = mainCntnr.innerHTML;
+	var ftrStr = '<footer id="footer">'+
+					'<p>Copyright 2019 Zaeem Technologies</p>'+
+				 '</footer>';
+	
+	mainCntnr.innerHTML = prevInnerHTML + ftrStr;	
+}
---
+++ js/training_list.js
@@ -0,0 +1,28 @@
+trng_lst= [
+	{
+		img: "http://localhost/zt/fonts/training.svg", 
+		title: "Block Chain Introduction",
+		desc: "An introduction to Block Chain <br/>&nbsp;"
+	},
+	{
+		img: "http://localhost/zt/fonts/training.svg", 
+		title: "AI/ML Introduction <br/>&nbsp;",
+		desc: "An introduction to AI/ML<br/>&nbsp;"
+	},
+	{
+		img: "http://localhost/zt/fonts/training.svg", 
+		title: "Cloud Computing Introduction",
+		desc: "An introduction to Cloud Computing"
+	},
+	{
+		img: "http://localhost/zt/fonts/training.svg", 
+		title: "AI/ML Introduction <br/>&nbsp;",
+		desc: "An introduction to AI/ML<br/>&nbsp;"
+	},
+	{
+		img: "http://localhost/zt/fonts/training.svg", 
+		title: "Cloud Computing Introduction",
+		desc: "An introduction to Cloud Computing"
+	},
+]
+
